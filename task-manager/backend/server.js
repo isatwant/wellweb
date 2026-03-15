1c1eb081e6191439bc46addac3f54cef4f102487
Satwant <143707746+isatwant@users.noreply.github.com>
2026-03-15 04:14:08 +0000

    Update backend dependencies and server configuration

--- task-manager/backend/server.js
+++ task-manager/backend/server.js
@@ -1,6 +1,24 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require("cors")
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("Task Manager API Running");
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
